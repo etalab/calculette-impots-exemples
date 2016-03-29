a05c327d849cf903fda7b5761328313ff99c783d
Christophe Benz <christophe.benz@gmail.com>
2016-03-29 14:40:37 +0200

    Add port explicitely, change to 5010

--- calculette_impots_web_explorer/scripts/serve.py
+++ calculette_impots_web_explorer/scripts/serve.py
@@ -9,7 +9,7 @@ from calculette_impots_web_explorer import application
 
 def main():
     app = application.make_app()
-    app.run(debug=True)
+    app.run(debug=True, port=5010)
 
 
 if __name__ == '__main__':
