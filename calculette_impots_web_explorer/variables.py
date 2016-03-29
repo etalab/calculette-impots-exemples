347384587572110798cf4a2f8ccdb831fc4622c7
Christophe Benz <christophe.benz@gmail.com>
2016-03-29 17:22:44 +0200

    Add required attribute, handle no value server-side validation

--- calculette_impots_web_explorer/variables.py
+++ calculette_impots_web_explorer/variables.py
@@ -61,8 +61,11 @@ def variable(variable_name):
     for name, value in request.args.items():
         if name == 'historique':
             continue
-        if state.variables_definitions.is_saisie(name) and value:
-            saisie_variable_input_by_name[name] = value
+        if state.variables_definitions.is_saisie(name):
+            if value:
+                saisie_variable_input_by_name[name] = value
+            else:
+                input_error_by_name[name] = u"L'argument {!r} n'a pas de valeur.".format(name)
         else:
             input_error_by_name[name] = u"L'argument {!r} n'est pas une variable de saisie.".format(name)
 
