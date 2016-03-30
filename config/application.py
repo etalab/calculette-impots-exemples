4ad2881d85bb00d006778e01de46b9c8f5b95998
Christophe Benz <christophe.benz@gmail.com>
2016-03-30 18:21:10 +0200

    Add logging and emails

--- config/application.py
+++ config/application.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
+import sys
+
+from logging import Formatter, StreamHandler
 from logging.handlers import SMTPHandler
 import logging
 
@@ -9,11 +12,24 @@ from calculette_impots_web_explorer.application import app as application
 
 application.config.update({
     # Display application exceptions in Apache log.
-    'PROPAGATE_EXCEPTIONS': True,
+    # Disabled because logging handlers are not executed due to exception.
+    #'PROPAGATE_EXCEPTIONS': True,
 
     # Overload application instance config.
     'M_SOURCE_FILES_DIR_PATH': '/home/openfisca/calculette-impots-m-source-code/src',
     })
+
+
+# Log exceptions in /var/log/apache2/error.log
+
+stream_handler = StreamHandler(stream=sys.stderr)
+stream_handler.setFormatter(Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+    ))
+stream_handler.setLevel(logging.WARNING)
+application.logger.addHandler(stream_handler)
+
 
 # Setup email sending on application exception.
 
@@ -25,4 +41,15 @@ mail_handler = SMTPHandler(
     'calculette-impots-web-explorer failed',
     )
 mail_handler.setLevel(logging.ERROR)
+mail_handler.setFormatter(Formatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+
+Message:
+
+%(message)s
+'''))
 application.logger.addHandler(mail_handler)
