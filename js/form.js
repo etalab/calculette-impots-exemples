4f6de7d205234ad0d59811ce1e8c31e14631b47d
Mori <keitamori@gmail.com>
2016-04-02 12:43:47 +0200

    Submit form and compute it

--- js/form.js
+++ js/form.js
@@ -1,7 +1,17 @@
 $(function(){
     var submittedForm = null;
+    var inputValues = {V_ANREV:2014};
     $('#declaration-impot').submit(function(event){
-        submittedForm = $('#declaration-impot').serializeArray();
         event.preventDefault();
+        submittedForm = $('#declaration-impot').serializeArray();
+
+        console.dir(submittedForm);
+
+        for(element in submittedForm){
+            log(element);
+            inputValues[element.name] = parseFloat(element.value);
+        }
+        console.log(compute(inputValues));
+
     });
 });
