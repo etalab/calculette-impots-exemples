38660c8b85d1a2a924896a0dfb7a0389c1df6226
Christophe Benz <christophe.benz@gmail.com>
2016-08-08 18:50:23 +0200

    Allow to overload config by env variable abandon flask "instance"-based configs

--- calculette_impots_web_explorer/application.py
+++ calculette_impots_web_explorer/application.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
+import os
+
 from flask import Flask
 
 from . import views
 
 
-app = Flask('calculette_impots_web_explorer', instance_relative_config=True)
+app = Flask('calculette_impots_web_explorer')
 
 app.config.from_pyfile('config.py')
+
+# Read optional config from another file.
+env_variable_name = 'CALCULETTE_IMPOTS_WEB_EXPLORER_SETTINGS'
+if os.getenv(env_variable_name):
+    app.config.from_envvar(env_variable_name)
 
 app.route('/')(views.variables)
 app.route('/<variable_name_or_alias>')(views.variable)
